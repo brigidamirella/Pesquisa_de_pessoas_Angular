340c1e8270f0311fd4f3949d30d389e8d70e71c0
Brigida Mirella <brigidamirella@hotmail.com>
2021-09-21 16:21:42 -0300

    usando interpolação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meuprojeto';
+  nome = 'Marcelo';
+  idade = '25';
+
+  getIdade() {
+    return this.idade;
+  }
 }
